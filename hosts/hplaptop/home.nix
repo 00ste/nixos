12066a22533e039fb9f6d490377b4afdd167cc3e
00ste <amalberti75@gmail.com>
2026-04-21 11:19:48 +0200

    added kitty (wip)

--- hosts/hplaptop/home.nix
+++ hosts/hplaptop/home.nix
@@ -6,6 +6,10 @@
   home.username = "stefano";
   home.homeDirectory = "/home/stefano";
 
+	imports = [
+		../../home/programs/kitty
+	];
+	
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
