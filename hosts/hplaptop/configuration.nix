446268017ecb33a14aa32b3754d4442fa673bb5d
00ste <amalberti75@gmail.com>
2026-04-29 23:12:12 +0200

    put system packages back where they were

--- hosts/hplaptop/configuration.nix
+++ hosts/hplaptop/configuration.nix
@@ -10,7 +10,6 @@
     ../../main-user.nix
     inputs.home-manager.nixosModules.default
     ../../system/fonts.nix
-    ../../system.packages.nix
   ];
  
   # Allow unfree packages
@@ -113,6 +112,27 @@
 
   # Gnome already has its own SSH agent
   # programs.ssh.startAgent = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    tree
+    
+    python3
+    
+    clang
+    gcc
+    gnumake
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
 
